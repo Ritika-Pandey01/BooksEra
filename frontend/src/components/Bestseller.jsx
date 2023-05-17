ddaf34242fc898b3e3651145a74f653b4da89335
Ritika-Pandey01 <ritika.pandey2020@vitbhopal.ac.in>
2023-05-17 10:51:28 +0530

    Modified bestseller

--- frontend/src/components/Bestseller.jsx
+++ frontend/src/components/Bestseller.jsx
@@ -1,19 +1,21 @@
 import React,{useState,useEffect} from 'react';
 import axios from 'axios';
 
+// const {REACT_APP_BOOK_API}=process.env;
+
 const Bestseller = () => {
     const [books,setBook]=useState([]);
     useEffect(()=>{
         const fetchBest=async()=>{
-            const res=await axios.get(`https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=ANjvL37R7r52GQXew3rvP4zLFKGtkzc6`);
+            const res=await axios.get(`https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=${process.env.REACT_APP_BOOK_API}`);
             setBook(res.data.results.books);
-            console.log(res.data.results.books);
+            // console.log(res.data.results.books);
         }
         fetchBest()
     },[])
   return (
     <div>
-      <h1 className='bestList'>Best Sellers</h1>
+      <h1 className='bestList'>NYT Best Sellers</h1>
       <div className="books">
             {books.map((book) => {
               const {rank,title,author,book_image,description,amazon_product_url}=book
@@ -25,10 +27,10 @@ const Bestseller = () => {
                   </div>
                   
                   <div className='details'>
+                  <span className="author"><b>Rank:</b> {rank}</span>
                   <span className="author"><b>Author:</b> {author}</span>
-                  <br/>
                   <span className="desc"><b>Description:</b> {description}</span>
-                  <span className="link"> <a href={amazon_product_url}><b>Link</b></a> </span>
+                  <span className="link"> <a href={amazon_product_url}><b>Amazon Link</b></a> </span>
                   </div>
                 </article>
               )
